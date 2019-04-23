c6feee5435e18f612e1b40c1cf5684e60e14a39a
Bianca Alexandra Herle <bianca.alexandra.herle@intel.com>
2019-04-23 19:59:26 +0300

    Update save.ino

--- save.ino
+++ save.ino
@@ -18,12 +18,12 @@ void setup() {
   pinMode(10, OUTPUT);
   TCCR2A = (1<<COM2A1) | (1<<COM2B1) | (1<<WGM21) | (1<<WGM20);
   TCCR2B = (1<<CS22);
-  OCR2A = 0; //11
-  OCR2B = 0; //3
+  OCR2A = 128; //11
+  OCR2B = 128; //3
   TCCR1A = (1<<COM1A1) | (1<<COM1B1) | (1<<WGM12) | (1<<WGM10);
   TCCR1B = (1<<CS11) | (1<<CS10);
-  OCR1A = 180; //9 sau 10
-  OCR1B = 180; //9 sau 10
+  OCR1A = 128; //9 sau 10
+  OCR1B = 128; //9 sau 10
 
 }
 ISR(TIMER1_COMPA_vect){
